b7fc8dcacbdd92a9d32014a6f54d0ed88d6ed4a8
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-10-18 18:47:16 +0700

    chore: add utility to check isNil and isEqual

---
+++ must.go
@@ -0,0 +1,32 @@
+package must
+
+import (
+	"reflect"
+)
+
+
+func isNil(a interface{}) bool {
+	if a == nil {
+		return true
+	}
+
+	val := reflect.ValueOf(a)
+	kind := val.Kind()
+	if kind >= reflect.Chan && kind <= reflect.Slice && val.IsNil() {
+		return true
+	}
+
+	return false
+}
+
+func isEqual(a, b interface{}) bool {
+	if isNil(a) && isNil(b) {
+		return true
+	}
+
+	if reflect.DeepEqual(a, b) {
+		return true
+	}
+
+	return reflect.ValueOf(a) == reflect.ValueOf(b)
+}
