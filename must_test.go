91dd6524bddadc71cf63761777b17df9ca3a649e
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-02-12 18:57:49 +0700

    test: 🧪 improve test mechanism

--- must_test.go
+++ must_test.go
@@ -9,10 +9,6 @@ type mockT struct {
 	failed bool
 }
 
-func (m mockT) logAndFail(args ...interface{}) {
-	m.Fail()
-}
-
 func Test_Must(t *testing.T) {
 	t.Run("callerinfo", func(t *testing.T) {
 		must := New(t)
@@ -20,46 +16,58 @@ func Test_Must(t *testing.T) {
 		must.True(ok)
 	})
 
+	t.Run("logAndFail", func(t *testing.T) {
+		tt := &mockT{}
+		must := New(tt)
+		must.logAndFail()
+		False(t, tt.failed)
+	})
+
 	t.Run("Equal", func(t *testing.T) {
-		must := New(&mockT{})
+		tt := &mockT{}
+		must := New(tt)
 		must.Equal(1, 1)
-
-		must = New(&mockT{})
-		must.Equal(1, 2)
-
-		must = New(&mockT{})
 		must.Equal(&struct{}{}, &struct{}{})
+		False(t, tt.failed)
 	})
 
 	t.Run("Not Equal", func(t *testing.T) {
-		must := New(&mockT{})
+		tt := &mockT{}
+		must := New(tt)
 		must.NotEqual(1, 2)
-
-		must = New(&mockT{})
-		must.NotEqual(1, 1)
+		must.NotEqual(&struct{ i int }{1}, &struct{ i int }{2})
+		False(t, tt.failed)
 	})
 
 	t.Run("Nil", func(t *testing.T) {
-		must := New(t)
+		tt := &mockT{}
+		must := New(tt)
 		must.Nil(nil)
 
 		var b []byte
 		must.Nil(b)
+		False(t, tt.failed)
 	})
 
 	t.Run("Not Nil", func(t *testing.T) {
-		must := New(t)
+		tt := &mockT{}
+		must := New(tt)
 		must.NotNil(1)
 		must.NotNil(&struct{}{})
+		False(t, tt.failed)
 	})
 
 	t.Run("True", func(t *testing.T) {
+		tt := &mockT{}
 		must := New(t)
 		must.True(true)
+		False(t, tt.failed)
 	})
 
 	t.Run("False", func(t *testing.T) {
+		tt := &mockT{}
 		must := New(t)
 		must.False(false)
+		False(t, tt.failed)
 	})
 }
--- that_test.go
+++ that_test.go
@@ -3,9 +3,16 @@ package must
 import "testing"
 
 func Test_That(t *testing.T) {
-	that := That(&mockT{}, 1)
+	tt := &mockT{}
+	that := That(tt, 1)
 	that.NotNil().Equal(1)
 
-	that2 := That[*int](&mockT{}, nil)
+	that2 := That[*int](tt, nil)
 	that2.Nil().NotEqual(1)
+
+	that3 := That(tt, true)
+	that3.True()
+
+	that4 := That(tt, false)
+	that4.False()
 }
