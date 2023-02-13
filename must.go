9cf62de5ef80fb8ffc31ec773a763c80fb1aa6d8
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-02-13 09:27:10 +0700

    docs(must): 📄 per public things docs

--- must.go
+++ must.go
@@ -9,10 +9,14 @@ import (
 	"testing"
 )
 
+// Must contains single testing.TB instance
+// and it will be reuse in same test case
 type Must struct {
 	t testing.TB
 }
 
+// New need testing.TB as parameter
+// and Must instance will be returned
 func New(t testing.TB) *Must {
 	return &Must{
 		t: t,
@@ -73,30 +77,36 @@ func (m Must) logAndFail(args ...any) {
 	m.t.Fail()
 }
 
+// Equal check are a and b equal, if not it will be failed
 func (m Must) Equal(a, b any) {
 	if !isEqual(a, b) {
 		m.logAndFail(fmt.Sprintf("need %v, got %v", a, b))
 	}
 }
 
+// NotEqual reverse of Equal which is failed if a and b are equal
 func (m Must) NotEqual(a, b any) {
 	if isEqual(a, b) {
 		m.logAndFail(fmt.Sprintf("need %v, got %v", a, b))
 	}
 }
 
+// Nil check is a nil, if not it will be failed
 func (m Must) Nil(a any) {
 	m.Equal(nil, a)
 }
 
+// NotNil reverse of Nil which is failed if a nil
 func (m Must) NotNil(a any) {
 	m.NotEqual(nil, a)
 }
 
+// True check is a true, if not it will be failed
 func (m Must) True(a bool) {
 	m.Equal(true, a)
 }
 
+// False check is a false, if not it will be failed
 func (m Must) False(a bool) {
 	m.Equal(false, a)
 }
