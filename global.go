72f49c075c7d48034e03e20c611b73c03899dffb
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-02-13 09:21:50 +0700

    docs(global): 📄 per fn docs

--- global.go
+++ global.go
@@ -18,31 +18,37 @@ func (m *mustSync) setT(t testing.TB) {
 
 var globalMust = mustSync{m: &Must{}}
 
+// Equal check are a and b equal, if not it will be failed
 func Equal(t testing.TB, a, b any) {
 	globalMust.setT(t)
 	globalMust.m.Equal(a, b)
 }
 
+// NotEqual reverse of Equal which is failed if a and b are equal
 func NotEqual(t testing.TB, a, b any) {
 	globalMust.setT(t)
 	globalMust.m.NotEqual(a, b)
 }
 
+// Nil check is a nil, if not it will be failed
 func Nil(t testing.TB, a any) {
 	globalMust.setT(t)
 	globalMust.m.Nil(a)
 }
 
+// NotNil reverse of Nil which is failed if a nil
 func NotNil(t testing.TB, a any) {
 	globalMust.setT(t)
 	globalMust.m.NotNil(a)
 }
 
+// True check is a true, if not it will be failed
 func True(t testing.TB, a bool) {
 	globalMust.setT(t)
 	globalMust.m.True(a)
 }
 
+// False check is a false, if not it will be failed
 func False(t testing.TB, a bool) {
 	globalMust.setT(t)
 	globalMust.m.False(a)
